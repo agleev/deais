9257418d888a6c752b76c6987e74509e6ee783c1
Azat Agleev <82861838+agleev@users.noreply.github.com>
2025-09-10 10:01:17 +0300

    Update msg_type_5.py
    add ascii table, get str atributes ais_msg

--- msg_type_5.py
+++ msg_type_5.py
@@ -23,3 +23,34 @@ destination = bit_string[302:422]
 dte = bit_string[422:423]
 spare = bit_string[423:]
 """
+
+ascii_6_dict = {
+    0: '@',   1: 'A',   2: 'B',   3: 'C',   4: 'D',   5: 'E',   6: 'F',   7: 'G',
+    8: 'H',   9: 'I',  10: 'J',  11: 'K',  12: 'L',  13: 'M',  14: 'N',  15: 'O',
+    16: 'P',  17: 'Q',  18: 'R',  19: 'S',  20: 'T',  21: 'U',  22: 'V',  23: 'W',
+    24: 'X',  25: 'Y',  26: 'Z',  27: '[',  28: '\\', 29: ']',  30: '^',  31: '_',
+    32: ' ',  33: '!',  34: '"',  35: '#',  36: '$',  37: '%',  38: '&',  39: "'",
+    40: '(',  41: ')',  42: '*',  43: '+',  44: ',',  45: '-',  46: '.',  47: '/',
+    48: '0',  49: '1',  50: '2',  51: '3',  52: '4',  53: '5',  54: '6',  55: '7',
+    56: '8',  57: '9',  58: ':',  59: ';',  60: '<',  61: '=',  62: '>',  63: '?'
+}
+
+def get_ascii(value):
+  idx = int(value, 2)
+  return ascii_6_dict[idx&63]
+
+
+def proc_6bit_chank(binary_string: bytes):
+  if len(binary_string) % 6 !=0 :
+    raise ValueError
+
+    result=""
+    for i in range(0, len(binary_string), 6):
+        temp_6b = binary_string[i:i+6]
+        ascii_sym = ascii_from_6b(temp_6b)
+        result+=ascii_sym
+    return result
+
+
+
+
